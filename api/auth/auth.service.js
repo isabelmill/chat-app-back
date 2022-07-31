fb9d8bf7ae4f332054f124eb28bb9499d4c2bd4e
Isabel Mill <isabelmavd2@gmail.com>
2022-08-01 00:41:03 +0300

    setting up server

---
+++ api/auth/auth.service.js
@@ -0,0 +1,35 @@
+const bcrypt = require('bcrypt')
+const userService = require('../user/user.service')
+const logger = require('../../services/logger.service')
+
+
+async function login(email, password) {
+    logger.debug(`auth.service - login with email: ${email}`)
+    const user = await userService.getByEmail(email)
+    if (!user) return Promise.reject('Invalid email or password')
+    const match = await bcrypt.compare(password, user.password)
+    // if (!match || user.email !== email) return Promise.reject('Invalid email or password')
+    if (user.password !== password || user.email !== email) return Promise.reject('Invalid email or password')
+    delete user.password
+    user._id = user._id.toString()
+    return user
+}
+
+
+async function signup(username, password, fullname) {
+    const saltRounds = 10
+
+    logger.debug(`auth.service - signup with username: ${username}, fullname: ${fullname}`)
+    if (!username || !password || !fullname) return Promise.reject('fullname, username and password are required!')
+
+    const userExist = await userService.getByUsername(username)
+    if (userExist) return Promise.reject('Username already taken')
+
+    const hash = await bcrypt.hash(password, saltRounds)
+    return userService.add({ username, password: hash, fullname })
+}
+
+module.exports = {
+    signup,
+    login
+}
---
+++ config/prod.js
@@ -0,0 +1,3 @@
+module.exports = {
+  dbURL: process.env.MONGODB_URL,
+}
